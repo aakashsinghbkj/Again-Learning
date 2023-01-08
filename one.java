7dcd7b96bf6f6752d4f32b294089972a2b88b8c3
Akash <aakashsinghbkj@gmail.com>
2023-01-08 20:13:40 +0530

    initial commit

---
+++ one.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+class one{
+
+   public static void main(String [] args) {
+   Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the number");
+     int a = sc.nextInt();
+     System.out.println("number is " + a);
+   }
+
+}
