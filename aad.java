332f01e8950eeca31249b8173d43d9efc93bd639
Akash <aakashsinghbkj@gmail.com>
2023-01-09 09:21:27 +0530

    Second commit

---
+++ aad.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class aad {
+    public static void main(String [] args){
+        System.out.println("Enter the word");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println("Your word is " + a);
+
+    }
+}
